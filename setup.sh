f319cca930ee3446e7d77955a9b0e1be6014b237
keinsleif <mountaindull@gmail.com>
2024-05-13 17:07:17 +0900

    add zsh setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 
 dotfiles=(".zshrc" ".p10k.zsh" ".cargo/config.toml")
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 for file in "${dotfiles[@]}"; do
-        ln -sv dotfiles/$file ~/$file
+        ln -svf dotfiles/$file ~/$file
 done
