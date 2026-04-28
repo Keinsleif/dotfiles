adbdd7a7f6ee5012380fa5ecd20f4a0c2ee28ee3
Keinsleif <mountaindull@gmail.com>
2026-04-28 19:35:18 +0900

    update: setup script

--- setup.sh
+++ setup.sh
@@ -9,3 +9,18 @@ mkdir -pv ~/.config/sheldon
 for file in "${dotfiles[@]}"; do
         ln -svf ~/dotfiles/$file ~/$file
 done
+
+# setup rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+source ~/.zshrc
+
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+
+cargo binstall sccache
+
+# setup nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+
+NVM_DIR=/root/.nvm
+source $NVM_DIR/nvm.sh && nvm install --lts
