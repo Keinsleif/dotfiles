b878a2e3bb8991c7bd29b219d3c853b277628df0
Keinsleif <mountaindull@gmail.com>
2026-05-11 20:45:29 +0900

    migrate: mise

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 dotfiles=(".zshrc" ".p10k.zsh" ".cargo/config.toml" ".config/rsgain/presets/SBS.ini" ".gitconfig" ".nanorc" ".config/sheldon/plugins.toml")
 
@@ -10,20 +10,19 @@ for file in "${dotfiles[@]}"; do
         ln -svf ~/dotfiles/$file ~/$file
 done
 
-# setup rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
+# setup mise
+curl https://mise.run | sh
 source ~/.zshrc
+
+# setup rust
+mise use --global rust
 
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
 cargo binstall sccache
 
-# setup nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
-
-NVM_DIR=/root/.nvm
-source $NVM_DIR/nvm.sh && nvm install --lts
+# setup node
+mise use --global node@lts
 
 # setup pnpm
-curl -fsSL https://get.pnpm.io/install.sh | sh -
+mise use --global pnpm@10
